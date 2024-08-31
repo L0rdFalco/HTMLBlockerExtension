0cf31cb34d021a21a6e73bd1efdf127610a58ac3
tel3port <carn3lian@gmail.com>
2024-08-31 16:46:23 +0300

    more bg script refactoring

--- src/background.js
+++ src/background.js
@@ -1,14 +1,11 @@
-'use strict';
-
+/*
 import Mellowtel from "mellowtel";
-
 
 let mellowtel;
 (async () => {
-  mellowtel = new Mellowtel("34c8c438", { disableLogs: true });
-  // await mellowtel.initBackground();
+  mellowtel = new Mellowtel("b408b488", { disableLogs: true });
+  await mellowtel.initBackground();
 })();
-
 
 (async () => {
   let settingsLink = await mellowtel.generateSettingsLink()
@@ -18,127 +15,132 @@ let mellowtel;
 
 chrome.runtime.onInstalled.addListener(function (details) {
 
-  if (details.reason === "install") {
-
+  if (details.reason === "install" || details.reason === "update") {
     (async () => {
-      // await mellowtel.generateAndOpenOptInLink();
+      await mellowtel.generateAndOpenOptInLink();
     })();
   }
 
 });
 
+*/
+console.log("bg script loaded");
+
+let allowed = true;
+
 (() => {
   function _slicedToArray(t, n) { return _arrayWithHoles(t) || _iterableToArrayLimit(t, n) || _unsupportedIterableToArray(t, n) || _nonIterableRest() } function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.") } function _unsupportedIterableToArray(t, n) { if (t) { if ("string" == typeof t) return _arrayLikeToArray(t, n); var r = Object.prototype.toString.call(t).slice(8, -1); return "Object" === r && t.constructor && (r = t.constructor.name), "Map" === r || "Set" === r ? Array.from(t) : "Arguments" === r || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(r) ? _arrayLikeToArray(t, n) : void 0 } } function _arrayLikeToArray(t, n) { (null == n || n > t.length) && (n = t.length); for (var r = 0, e = new Array(n); r < n; r++)e[r] = t[r]; return e } function _iterableToArrayLimit(t, n) { var r = null == t ? null : "undefined" != typeof Symbol && t[Symbol.iterator] || t["@@iterator"]; if (null != r) { var e, o, u = [], i = !0, a = !1; try { for (r = r.call(t); !(i = (e = r.next()).done) && (u.push(e.value), !n || u.length !== n); i = !0); } catch (t) { a = !0, o = t } finally { try { i || null == r.return || r.return() } finally { if (a) throw o } } return u } } function _arrayWithHoles(t) { if (Array.isArray(t)) return t } function _typeof(t) { return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (t) { return typeof t } : function (t) { return t && "function" == typeof Symbol && t.constructor === Symbol && t !== Symbol.prototype ? "symbol" : typeof t }, _typeof(t) } !function (t, n) { "object" === ("undefined" == typeof exports ? "undefined" : _typeof(exports)) && "undefined" != typeof module ? n(exports) : "function" == typeof define && define.amd ? define(["exports"], n) : n((t = "undefined" != typeof globalThis ? globalThis : t || self).idbKeyval = {}) }(this, (function (t) { "use strict"; function n(t) { return new Promise((function (n, r) { t.oncomplete = t.onsuccess = function () { return n(t.result) }, t.onabort = t.onerror = function () { return r(t.error) } })) } function r(t, r) { var e = indexedDB.open(t); e.onupgradeneeded = function () { return e.result.createObjectStore(r) }; var o = n(e); return function (t, n) { return o.then((function (e) { return n(e.transaction(r, t).objectStore(r)) })) } } var e; function o() { return e || (e = r("keyval-store", "keyval")), e } function u(t, r) { return t.openCursor().onsuccess = function () { this.result && (r(this.result), this.result.continue()) }, n(t.transaction) } t.clear = function () { var t = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : o(); return t("readwrite", (function (t) { return t.clear(), n(t.transaction) })) }, t.createStore = r, t.del = function (t) { var r = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : o(); return r("readwrite", (function (r) { return r.delete(t), n(r.transaction) })) }, t.delMany = function (t) { var r = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : o(); return r("readwrite", (function (r) { return t.forEach((function (t) { return r.delete(t) })), n(r.transaction) })) }, t.entries = function () { var t = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : o(); return t("readonly", (function (r) { if (r.getAll && r.getAllKeys) return Promise.all([n(r.getAllKeys()), n(r.getAll())]).then((function (t) { var n = _slicedToArray(t, 2), r = n[0], e = n[1]; return r.map((function (t, n) { return [t, e[n]] })) })); var e = []; return t("readonly", (function (t) { return u(t, (function (t) { return e.push([t.key, t.value]) })).then((function () { return e })) })) })) }, t.get = function (t) { var r = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : o(); return r("readonly", (function (r) { return n(r.get(t)) })) }, t.getMany = function (t) { var r = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : o(); return r("readonly", (function (r) { return Promise.all(t.map((function (t) { return n(r.get(t)) }))) })) }, t.keys = function () { var t = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : o(); return t("readonly", (function (t) { if (t.getAllKeys) return n(t.getAllKeys()); var r = []; return u(t, (function (t) { return r.push(t.key) })).then((function () { return r })) })) }, t.promisifyRequest = n, t.set = function (t, r) { var e = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : o(); return e("readwrite", (function (e) { return e.put(r, t), n(e.transaction) })) }, t.setMany = function (t) { var r = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : o(); return r("readwrite", (function (r) { return t.forEach((function (t) { return r.put(t[1], t[0]) })), n(r.transaction) })) }, t.update = function (t, r) { var e = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : o(); return e("readwrite", (function (e) { return new Promise((function (o, u) { e.get(t).onsuccess = function () { try { e.put(r(this.result), t), o(n(e.transaction)) } catch (t) { u(t) } } })) })) }, t.values = function () { var t = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : o(); return t("readonly", (function (t) { if (t.getAll) return n(t.getAll()); var r = []; return u(t, (function (t) { return r.push(t.value) })).then((function () { return r })) })) }, Object.defineProperty(t, "__esModule", { value: !0 }) }));
 })()
 
-let allowed = true
-
 
 function onIcon() {
-  chrome.action.setIcon({ path: "./icons/icon16_on.png" })
-  chrome.action.setTitle({ title: "blocking on" })
+  chrome.action.setIcon({ path: "./icons/icon16_on.png" });
+  chrome.action.setTitle({ title: "ON" })
+
 }
 
 function offIcon() {
-  chrome.action.setIcon({ path: "./icons/icon16_off.png" })
-  chrome.action.setTitle({ title: "blocking off" })
+  chrome.action.setIcon({ path: "./icons/icon16_off.png" });
+  chrome.action.setTitle({ title: "OFF" });
+
 }
 
 function noIcon() {
-  chrome.action.setIcon({ path: "./icons/icon16_no.png" })
-  chrome.action.setTitle({ title: "blocking forbidden!" })
-}
 
+  chrome.action.setIcon({ path: "./icons/icon16_no.png" });
+  chrome.action.setTitle({ title: "NO!" })
+}
 
 async function isSiteViable() {
-
-  let tabs = await chrome.tabs.query({ active: true, currentWindow: true })
-
-  if (!tabs || !tabs.length || tabs[0].id < 0) return // not a tab you'd need the blocker to work
+  let tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+  if (!tabs || !tabs.length || tabs[0].id < 0) return;
 
   return tabs[0];
-}
-
-async function toggleBlocker(mTab) {
-  await chrome.tabs.sendMessage(mTab.id, { action: "toggle", status: allowed })
-
 
 }
 
-/*
-is blocker window visible
-shows appropriate icons based on blocking status and type of page
-*/
 async function isBlockingOn() {
 
-  let mTab = await isSiteViable()
+  try {
+    let mTab = await isSiteViable(); // execution stops if there's no tab object
 
-  if (!mTab.url.startsWith("http")) {
-    return noIcon();
+    if (!mTab.url.startsWith("http")) return noIcon(); // execution stops after icon is set
+
+    let blockStatus = await chrome.tabs.sendMessage(mTab.id, { action: "getStatus" })
+
+    console.log("blocking status: ", blockStatus);
+
+    blockStatus ? onIcon() : offIcon();
+
+  } catch (error) {
+    console.log(error);
+    console.log("blocking status error");
+
   }
 
-  let blockStatus = await chrome.tabs.sendMessage(mTab.id, { action: "getStatus" }).catch(() => {
-    console.log("tabs send message fail 2");
-  })
+}
 
-  console.log("blocking? ", blockStatus);
 
-  blockStatus ? onIcon() : offIcon()
+async function toggleBlocker(mTabID) {
+
+  try {
+    await chrome.tabs.sendMessage(mTabID, { action: "toggle", status: allowed })
+
+  } catch (error) {
+    console.log("toggle error");
+
+  }
+
+
 }
 
 async function forceInjectCS(mTab) {
-  try {
-    console.log("injecting content script");
-    // force inject cs
-    await chrome.scripting.executeScript({ files: ["content_script.js"], target: { tabId: mTab.id } })
 
-    //set icon  to active
-    await toggleBlocker(mTab)
+  try {
+
+    await chrome.scripting.executeScript({ files: ["content_script.js"], target: { tabId: mTab.id } })
   } catch (error) {
-    console.log("could not load on this protected page");
+    console.log("the webpage is probably forbidding script injection");
     noIcon()
+
   }
+
+
 }
+
 
 chrome.action.onClicked.addListener(async function () {
-  /*
-  1. check if its a real tab
-  2. check if extension is activated on said tab by sending msg to content script
-  3. if not activated, load the content scripts that activates the blocker
-  */
+  try {
+    const mTab = await isSiteViable();
 
-  let mTab = await isSiteViable();
+    const res = await toggleBlocker(mTab.id)
 
-  await toggleBlocker(mTab)
+    if (!res) { // in the event the browser didn't inject the CS from the jump
+      await forceInjectCS(mTab)
 
+      await toggleBlocker(mTab.id)
 
-  if (!res) { // means that content script wasn't injected there4 do so dynamically
+    }
 
-    await forceInjectCS(mTab)
+  } catch (error) {
 
-  }
-
-}
-)
-
-chrome.tabs.onActivated.addListener((data) => {
-  console.log("tabs on activated");
-  isBlockingOn()
-})
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  console.log("tabs on updated");
-
-  isBlockingOn()
-})
-
-chrome.runtime.onMessage.addListener((msg, sender, res) => {
-
-  if (msg.action === "checkStatus") {
-    msg.blocking ? onIcon() : offIcon()
+    console.log("icon click error");
 
   }
 
+
 })
+
+chrome.tabs.onActivated.addListener((tabId, changeInfo, tab) => {
+  isBlockingOn()
+
+})
+
+chrome.tabs.onUpdated.addListener((msg, sender, res) => {
+
+  isBlockingOn()
+
+})
+
 
 isBlockingOn()
