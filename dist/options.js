3409de173442159ea3c1735cf8e4c40198016760
tel3port <carn3lian@gmail.com>
2024-09-23 17:23:16 +0300

    added function shells in options.js

--- dist/options.js
+++ dist/options.js
@@ -35,4 +35,27 @@ window.onload = function () {
     loadPrefs()
     showRules()
 
+    document.getElementById("contextMenu").onclick = save
+    document.getElementById("autoRefresh").onclick = save
+    document.getElementById("allSubdomains").onclick = save
+    document.getElementById("allPorts").onclick = save
+    document.getElementById("lightIcon").onclick = save
+    document.getElementById("defaultIcon").onclick = save
+    document.getElementById("darkIcon").onclick = save
+
+    document.getElementById("openImageSettings").onclick = () => {
+
+    }
+    document.getElementById("clearImageSettings").onclick = () => {
+
+    }
+    document.getElementById("importRules").onclick = () => {
+
+    }
+    document.getElementById("clearLocalStorageRules").onclick = () => {
+
+    }
+
+
+
 }
